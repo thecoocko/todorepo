0771d7b3c6d3f7020e2759a4af23454131dde2de
coocko <krabik4794@gmail.com>
2021-03-13 17:44:27 +0200

    feat: add possibility to create and add new task

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const input = document.getElementById("todo");
 const close = document.getElementsByClassName("close");
 const li = document.getElementsByClassName("to-do");
+const btn = document.getElementById("btn");
+const list = document.getElementById("todo-list");
 
-
+btn.addEventListener("click", ()=>{
+    let li = document.createElement("li");
+    let value = input.value;
+    let task = document.createTextNode(value);
+    li.appendChild(task);
+    input.innerText = "";
+    li.classList.add("divider");
+    list.appendChild(li);
+    console.log(task)
+});
